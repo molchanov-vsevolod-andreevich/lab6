87e4877201afe1d9152172a5feec1fc61db2cb2c
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-14 11:10:42 +0300

    Scripted auto-commit on change (2019-12-14 11:10:42) by gitwatch.sh

--- src/main/java/ZookeeperApp.java
+++ src/main/java/ZookeeperApp.java
@@ -1,24 +1,12 @@
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.ZooDefs;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
+import java.io.IOException;
 import java.util.List;
 
 public class ZookeeperApp {
 
     public static void main(String[] args) throws KeeperException, InterruptedException {
-        ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181", 3000, this);
-        zoo.create("/servers/s",
-                "data".getBytes(),
-                ZooDefs.Ids.OPEN_ACL_UNSAFE,
-                CreateMode.EPHEMERAL_SEQUENTIAL
-        );
-        List<String> servers = zoo.getChildren("/servers", this);
-        for (String s : servers) {
-            byte[] data = zoo.getData("/servers/" + s, false, null);
-            System.out.println("server " + s + " data=" + new String(data));
-        }
+        test();
 //        System.out.println(AkkaStreamsAppConstants.START_MESSAGE);
 //
 //        ActorSystem system = ActorSystem.create(AkkaStreamsAppConstants.ACTOR_SYSTEM_NAME);
@@ -41,6 +29,20 @@ public class ZookeeperApp {
 //        binding
 //                .thenCompose(ServerBinding::unbind)
 //                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+    }
+
+    void test() throws IOException, KeeperException, InterruptedException {
+        ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181", 3000, (Watcher) this);
+        zoo.create("/servers/s",
+                "data".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL_SEQUENTIAL
+        );
+        List<String> servers = zoo.getChildren("/servers", (Watcher) this);
+        for (String s : servers) {
+            byte[] data = zoo.getData("/servers/" + s, false, null);
+            System.out.println("server " + s + " data=" + new String(data));
+        }
     }
 
 }
