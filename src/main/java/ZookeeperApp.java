86a4e52a431be23257ec24803a668370b6d818c5
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-14 11:14:25 +0300

    Scripted auto-commit on change (2019-12-14 11:14:25) by gitwatch.sh

--- src/main/java/ZookeeperApp.java
+++ src/main/java/ZookeeperApp.java
@@ -1,48 +1,36 @@
-import org.apache.zookeeper.*;
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
 
-import java.io.IOException;
-import java.util.List;
+import java.util.concurrent.CompletionStage;
 
 public class ZookeeperApp {
 
-    public static void main(String[] args) throws KeeperException, InterruptedException {
-        test();
-//        System.out.println(AkkaStreamsAppConstants.START_MESSAGE);
-//
-//        ActorSystem system = ActorSystem.create(AkkaStreamsAppConstants.ACTOR_SYSTEM_NAME);
-//
-//        final Http http = Http.get(system);
-//        final ActorMaterializer materializer = ActorMaterializer.create(system);
-//
-//        AsyncHttpClient asyncHttpClient = Dsl.asyncHttpClient();
-//
-//        HttpRouter instance = new HttpRouter(system);
-//
-//        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRouteFlow(asyncHttpClient, materializer);
-//        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-//                routeFlow,
-//                ConnectHttp.toHost(AkkaStreamsAppConstants.HOST, AkkaStreamsAppConstants.PORT),
-//                materializer
-//        );
-//        System.out.println(AkkaStreamsAppConstants.START_SERVER_MESSAGE);
-//        System.in.read();
-//        binding
-//                .thenCompose(ServerBinding::unbind)
-//                .thenAccept(unbound -> system.terminate()); // and shutdown when done
-    }
+    public static void main(String[] args) {
+        System.out.println(ZookeeperAppConstants.START_MESSAGE);
 
-    void test() throws IOException, KeeperException, InterruptedException {
-        ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181", 3000, (Watcher) this);
-        zoo.create("/servers/s",
-                "data".getBytes(),
-                ZooDefs.Ids.OPEN_ACL_UNSAFE,
-                CreateMode.EPHEMERAL_SEQUENTIAL
+        ActorSystem system = ActorSystem.create(ZookeeperAppConstants.ACTOR_SYSTEM_NAME);
+
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+        HttpRouter instance = new HttpRouter(system);
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRouteFlow(asyncHttpClient, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(ZookeeperAppConstants.HOST, ZookeeperAppConstants.PORT),
+                materializer
         );
-        List<String> servers = zoo.getChildren("/servers", (Watcher) this);
-        for (String s : servers) {
-            byte[] data = zoo.getData("/servers/" + s, false, null);
-            System.out.println("server " + s + " data=" + new String(data));
-        }
+        System.out.println(ZookeeperAppConstants.START_SERVER_MESSAGE);
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); // and shutdown when done
     }
-
 }
