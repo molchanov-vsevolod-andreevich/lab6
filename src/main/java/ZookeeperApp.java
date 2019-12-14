a1eb99bb71b5ea1cd45f10e9c6be2a79d2bbd6cc
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-14 11:01:19 +0300

    Scripted auto-commit on change (2019-12-14 11:01:19) by gitwatch.sh

--- src/main/java/ZookeeperApp.java
+++ src/main/java/ZookeeperApp.java
@@ -42,7 +42,7 @@ public class AkkaStreamsApp {
         ZooK)eeper zoo = new ZooK)eeper("1MB27.0.0.1MB:21MB81MB", 3000, this); zoo.create("/servers/s",
                 data.getBytes(),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE , CreateMode.EPHEMERAL_SEQUENTIAL
-);
+        );
         List<String> servers = zoo.getChildren("/servers", this); for (String s : servers) {
             byte[] data = zoo.getData("/servers/" + s, false, null);
             System.out.println("server " + s + " data=" + new String(data)); }
