86be74a895fd47f51a66b7be2529273b01875760
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-28 05:54:36 +0300

    Scripted auto-commit on change (2019-12-28 05:54:36) by gitwatch.sh

--- src/main/java/ZookeeperClass.java
+++ src/main/java/ZookeeperClass.java
@@ -1,16 +1,25 @@
 import akka.actor.ActorRef;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
-public class ZookeeperClass {
+import java.io.IOException;
+
+public class ZookeeperClass implements Watcher {
     private ZooKeeper zooKeeper;
     private ActorRef cacheActor;
 
-    public ZookeeperClass(ActorRef cacheActor) {
+    public ZookeeperClass(ActorRef cacheActor) throws IOException {
         this.cacheActor = cacheActor;
         zooKeeper = new ZooKeeper(
                 ZookeeperAppConstants.ZOOKEEPER_SERVER + ":" + ZookeeperAppConstants.ZOOKEEPER_PORT,
                 ZookeeperAppConstants.ZOOKEEPER_SESSION_TIMEOUT,
-                watchedEvent -> String
-        )
+                this
+        );
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        
     }
 }
