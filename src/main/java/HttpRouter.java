f2f1e980b0b1b18f40abf980773189eabad44988
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-28 04:42:45 +0300

    Scripted auto-commit on change (2019-12-28 04:42:45) by gitwatch.sh

--- src/main/java/HttpRouter.java
+++ src/main/java/HttpRouter.java
@@ -5,6 +5,7 @@ import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.*;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
+import akka.japi.Pair;
 import akka.pattern.Patterns;
 import scala.concurrent.Future;
 
@@ -27,7 +28,7 @@ class HttpRouter extends AllDirectives {
                                     if (redirectCount != 0) {
                                         return completeOKWithFuture(curlUrl(http, url));
                                     } else {
-                                        return completeOKWithFuture(redirect(http, url, count));
+                                        return completeOKWithFuture(redirect(http, url, redirectCount));
                                     }
                                 }))));
     }
@@ -36,13 +37,16 @@ class HttpRouter extends AllDirectives {
         return http.singleRequest(HttpRequest.create(url));
     }
 
-    private CompletionStage<HttpResponse> redirect(Http http, String url, String count) {
+    private CompletionStage<HttpResponse> redirect(Http http, String url, int count) {
         return Patterns.ask(cacheActor, new CacheActor.GetRandomServer(), ZookeeperAppConstants.TIMEOUT)
+                .thenCompose(randServer -> )
                 .thenCompose(randServer -> {
                     String redirectUrl = Uri.create(randServer)
                             .query(Query.create(
-
+                                    Pair.create(ZookeeperAppConstants.URL_PARAMETER_NAME, url),
+                                    Pair.create(ZookeeperAppConstants.COUNT_PARAMETER_NAME, Integer.toString(count - 1))
                             ))
+                            .toString();
                     return curlUrl(http, redirectUrl);
                 })
     }
