3ffe5577ade0cdb7ae9980b7da57040cb55100b6
Vsevolod Molchanov <vsevolodmolchanov@gmail.com>
2019-12-28 05:56:46 +0300

    Scripted auto-commit on change (2019-12-28 05:56:46) by gitwatch.sh

--- src/main/java/ZookeeperClass.java
+++ src/main/java/ZookeeperClass.java
@@ -20,6 +20,6 @@ public class ZookeeperClass implements Watcher {
 
     @Override
     public void process(WatchedEvent watchedEvent) {
-        if (watchedEvent.getType())
+        if (watchedEvent.getType() == )
     }
 }
